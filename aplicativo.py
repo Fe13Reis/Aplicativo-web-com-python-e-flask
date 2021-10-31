189cf39554fd5710ce5a2eaf35b0764fc17d7f14
winzord <winzordoricent@gmail.com>
2021-10-31 14:32:35 -0300

    Corrigir bug sessão e possibilitar criação conta

--- aplicativo.py
+++ aplicativo.py
@@ -20,22 +20,7 @@ auth = firebase.auth()
 
 app = Flask(__name__)
 app.secret_key = 'alura'
-'''
-class Usuario:
-    def __init__(self, id, nome, senha):
-        self.id = id
-        self.nome = nome
-        self.senha = senha
 
-
-usuario1 = Usuario('fernando@gmail.com', 'Fernando', '1234')
-usuario2 = Usuario('winzor@gmail.com', 'Winzor', '5678')
-usuario3 = Usuario('thiago@gmail.com', 'Tiago', '9101')
-
-usuarios = {usuario1.id: usuario1,
-            usuario2.id: usuario2,
-            usuario3.id: usuario3}
-'''
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -44,39 +29,42 @@ def index():
 def agenda():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('agenda')))
-    return render_template('agenda.html')
+    return render_template('agenda.html', email = session['usuario_logado'])
 
 @app.route('/login')
 def login():
     proxima = request.args.get('proxima')
     return render_template('login.html', proxima=proxima)
 
-@app.route('/autenticar', methods=['POST', ])
+@app.route('/autenticar', methods=['POST', 'GET'])
 def autenticar():
-
-    '''
-    if request.form['e-mail'] in usuarios:
-        usuario = usuarios[request.form['e-mail']]
-        if usuario.senha == request.form['senha']:
-            session['usuario_logado'] = usuario.id
-            flash(usuario.nome + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(url_for('agenda'))
-        else:
-          flash('Não logado, tente novamente')
-          return redirect(url_for('login'))
-    '''
     if request.method == 'POST':
         email = request.form.get('e-mail')
         senha = request.form.get('senha')
         try:
             auth.sign_in_with_email_and_password(email, senha)
-            flash(email + ' logou com sucesso!')
-            #proxima_pagina = request.form['proxima']
+            session['usuario_logado'] = email
+            flash('Bem vinv@: '+ email + '!')
+            proxima_pagina = request.form['proxima']
             return redirect(url_for('agenda'))
         except:
-          flash('Não logado, tente novamente')
+          flash('Email e/ou senha, tente novamente')
           return redirect(url_for('login'))
+
+@app.route('/cadastrar', methods=['POST', 'GET'])
+def cadastrar():
+    if request.method == 'POST':
+        email = request.form.get('e-mail')
+        senha = request.form.get('senha')
+        try:
+            auth.create_user_with_email_and_password(email, senha)
+            session['usuario_logado'] = email
+            flash('Bem vinv@: '+ email + '!')
+            proxima_pagina = request.form['proxima']
+            return redirect(url_for('agenda'))
+        except:
+          flash('Erro no Cadastro, tente novamente')
+          return redirect(url_for('cadastro'))
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
