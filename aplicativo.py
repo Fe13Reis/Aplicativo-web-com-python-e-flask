aea96ee21918092cc8557785bedacb0f320334fd
winzord <winzordoricent@gmail.com>
2021-11-06 17:52:02 -0300

    ajustes em agenda, index, login e aplicativo

--- aplicativo.py
+++ aplicativo.py
@@ -1,5 +1,12 @@
+import sqlite3
 from flask import Flask, render_template, redirect, request, session, flash, \
 url_for
+from werkzeug.exceptions import abort
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 import pyrebase
@@ -22,48 +29,22 @@ db = firebase.database();
 app = Flask(__name__)
 app.secret_key = 'alura'
 
-events = [
-    {
-        'todo' : 'Casa Cintia - Churrasco', 
-        'date' : '2021-11-02',
-    },
-    {
-        'todo' : 'Casa Eric - Cerveja', 
-        'date' : '2021-11-10',
-    },
-    {
-        'todo' : 'Casa Bruno - Piscina', 
-        'date' : '2021-11-15',
-    },
-    {
-        'todo' : 'Dia de Piscina', 
-        'date' : '2021-11-20',
-    }
-]
-
-#events = db.child('events').get()
+#recuperar todos os eventos do firebase
+#events = db.child('events').order_by_child('date').get()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/churras')
-def churras():
-    return render_template('agenda-churras.html', events = events)
-
-@app.route('/piscina')
-def piscina():
-    return render_template('agenda-piscina.html')
-
-@app.route('/salao')
-def salao():
-    return render_template('agenda-salao.html')
-
 @app.route('/agenda')
 def agenda():
+    conn = get_db_connection()
+    events = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('agenda')))
-    return render_template('agenda.html', email = session['usuario_logado'])
+        return redirect(url_for('login', proxima=url_for('login')))
+    #return render_template('agenda.html', email = session['usuario_logado'], events = events)
+    return render_template('agenda.html', email = session['usuario_logado'], events = events)
 
 @app.route('/login')
 def login():
@@ -99,6 +80,7 @@ def cadastrar():
         except:
           flash('Erro no Cadastro, tente novamente')
           return redirect(url_for('cadastro'))
+          
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
@@ -109,8 +91,7 @@ def logout():
 def cadastro():
     return render_template('cadastro.html')
 
-#data={'name' : 'Teste', 'numero':123, 'lgpd': True}
-#db.push(data)
+'''
 @app.route('/agendar-espaco', methods=['POST', 'GET'])
 def agendarEspaco():
     if request.method == 'POST':
@@ -124,5 +105,84 @@ def agendarEspaco():
         except:
           flash('Falha no Agendamento, tente novamente!')
           return redirect(url_for('churras'))
-#print(db.child('events').get())
+'''
+#Gravar um agendamento
+@app.route('/agendar-espaco', methods=('GET', 'POST'))
+def agendarEspaco():
+    if request.method == 'POST':
+        title = request.form['data']
+        content = request.form['espaco']
+
+        if not title:
+            flash('Escolhe a data, por favor!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('agenda'))
+
+    return render_template('agenda.html')
+
+#Recuperar todos os agendamentos
+@app.route('/dataMarcada')
+def dataMarcada():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('datas-marcadas.html',  posts=posts)
+
+#Possibilitar acessar a um registro (post)
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+#Acessar a um registro (post) para editar   
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('A data é obligatória!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('dataMarcada'))
+
+    return render_template('edit.html', post=post)
+
+#Deletar um agendamento
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" O agendamento foi deletado!'.format(post['title']))
+    return redirect(url_for('dataMarcada'))
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
 app.run(debug=True)
